c2390fd23375988fc5867bccc767758f8b789224
bradleydevilliers <ST10439310@vcconnect.edu.za>
2026-04-18 13:24:38 +0200

    created DBcontext with fluent API

---
+++ TechMoveGLMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using TechMoveGLMS.Models.Entities;
+
+namespace TechMoveGLMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        { 
+        }
+        
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<ServiceRequest> ServiceRequests { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Configure relationships using Fluent API
+            modelBuilder.Entity<Contract>()
+                .HasOne(c => c.Client)
+                .WithMany(c => c.Contracts)
+                .HasForeignKey(c => c.ClientId)
+                .OnDelete(DeleteBehavior.Cascade);
+                
+            modelBuilder.Entity<ServiceRequest>()
+                .HasOne(s => s.Contract)
+                .WithMany(c => c.ServiceRequests)
+                .HasForeignKey(s => s.ContractId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
