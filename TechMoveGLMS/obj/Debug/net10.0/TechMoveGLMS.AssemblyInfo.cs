ad845a8c7cddde3538b153c8dd6cd0770f997e51
bradleydevilliers <ST10439310@vcconnect.edu.za>
2026-04-22 09:26:44 +0200

    Redesigned the Register page

--- TechMoveGLMS/obj/Debug/net10.0/TechMoveGLMS.AssemblyInfo.cs
+++ TechMoveGLMS/obj/Debug/net10.0/TechMoveGLMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechMoveGLMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ad2030eec02500e5b08491e9826f94ebbe9ad9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8be2dcb483b29ceb5a7e85f0ed6d5d345e7325")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechMoveGLMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechMoveGLMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
